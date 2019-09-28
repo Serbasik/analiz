d242979c5d7157b1ca3bb8add324852a1100685d
Serbas <snslast@mail.ru>
2019-09-28 11:37:10 +0300

    start

---
+++ views/site/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Система анализа показателей детской смертности и рождаемости';
+?>
+    <div class="site-index">
+
+        <div class="jumbotron">
+            <h3>Добро пожаловать в систему анализа "Морталити"</h3>
+
+
+        </div>
+        <div id="map"></div>
+        <div class="body-content">
+
+            <div class="row">
+
+            </div>
+
+        </div>
+    </div>
+<?php
